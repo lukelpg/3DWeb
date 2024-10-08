76cb6680fd8aa95ab5511b2ee427b2c709adddce
lukelpg <20lapg1@queensu.ca>
2024-10-07 21:21:19 -0400

    Added a simple menu page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'; // Optional CSS file for styling
 
+import HomeMenu from './components/UI/homeMenu';
 import Toolbar from './components/toolBar';
 import WorldScene  from './components/world';
 // import FetchData from './utils/FetchData';
 
 function App() {
+  const [showMenu, setShowMenu] = useState(true);
+
+  const handleStart = () => {
+      setShowMenu(false);
+  };
+
+  const handleSettings = () => {
+      alert("Settings clicked!"); // Placeholder for settings functionality
+  };
+
+  const handleExit = () => {
+      // Handle exit logic, if needed
+      alert("Exit clicked!"); // Placeholder for exit functionality
+  };
+
+
   const handleAction1 = () => {
     console.log('Action 1 triggered');
     // Add your action logic here
@@ -22,11 +39,18 @@ function App() {
         <h1>3D Web</h1>
       </header> */}
       <main>
-        <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
-            <WorldScene />
-            <Toolbar onAction1={handleAction1} onAction2={handleAction2} />
-        </div>
-        
+            {showMenu ? (
+                <HomeMenu 
+                    onStart={handleStart} 
+                    onSettings={handleSettings} 
+                    onExit={handleExit} 
+                />
+            ) : (
+              <div style={{ position: 'relative', height: '100vh', width: '100vw' }}>
+                <WorldScene />
+                <Toolbar onAction1={handleAction1} onAction2={handleAction2} />
+              </div>
+            )}
         {/* <FetchData /> */}
       </main>
     </div>
